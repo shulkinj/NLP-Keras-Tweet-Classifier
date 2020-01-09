4b1df07b676ec606498daabfcbe0c8704c3d086c
jshulkin <jshulkin@umich.edu>
2020-01-09 15:24:22 -0500

    finished word preprocessing, next start model

--- nlp_model.py
+++ nlp_model.py
@@ -20,8 +20,9 @@ class NLP_Model():
         
         #constructs dictionary
         #loads training tweets & markings into training data for network
-        self.word_index, self.reversed_word_index = self.preprocess(self.dict_max_length) 
-
+        self.word_index, self.reversed_word_index = self.build_dict(self.dict_max_length) 
+        (self.x_train,self.y_train),(self.x_test, self.y_test) = self.preprocess()
+      
 
 
     def load_data(self, split_prop):
@@ -45,8 +46,8 @@ class NLP_Model():
 
     ## Constructs dictionary
     ## Optional dictionary max length by word fequency in training data
-    def preprocess(self, max_dict_size=float('inf')):
-        print('PREPROCESSING...')
+    def build_dict(self, max_dict_size=float('inf')):
+        print('BUILDING DICTIONARY...')
         
         training_tweets = self.trainDF[['text']].values
         
@@ -83,8 +84,58 @@ class NLP_Model():
         
         reversed_word_index = dict([(v,k) for (k,v) in word_index.items()])
         
-        return word_index, reversed_word_index
 
+
+        print("Dictionary size: ",len(word_index)," words ")
+        return word_index, reversed_word_index
+    
+
+
+    ## Returns an encoding vector for inputted tweet corresponding to word_index
+    def encode(self,tweet):
+        split_up = tweet_word_parse(tweet)
+        encoding = []
+        for word in split_up:
+            if(word in self.word_index):
+                encoding.append(self.word_index[word])
+            else:
+                encoding.append(0)
+        return encoding
+    
+
+    ## Outputs train and test inputs and outputs for use in model
+    def preprocess(self):
+        print("PREPROCESSING...")
+        ## Encodes all train and test tweets into tweet vectors, words corresponding to indices
+        x_train = []
+        x_test = []
+        train_tweets=self.trainDF[['text']].values
+        test_tweets=self.testDF[['text']].values
+        for tweet in train_tweets:
+            x_train.append(self.encode(tweet[0]))
+        for tweet in test_tweets:
+            x_test.append(self.encode(tweet[0]))
+
+        y_train = []
+        y_test = []
+        train_markings=self.trainDF[['handle']].values
+        test_markings=self.testDF[['handle']].values
+        for marking in train_markings:
+            if marking[0]=='realDonaldTrump':
+                y_train.append(0)
+            elif marking[0]=='HillaryClinton':
+                y_train.append(1)
+
+        for marking in test_markings:
+            if marking[0]=='realDonaldTrump':
+                y_test.append(0)
+            elif marking[0]=='HillaryClinton':
+                y_test.append(1)
+
+
+        return (x_train,y_train) , (x_test,y_test)
+
+    
 
 
 
@@ -109,7 +160,7 @@ class NLP_Model():
 def tweet_word_parse(tweet):
     import re
     return re.sub(r'[^a-zA-Z ]', '',tweet).lower().split(" ")
-       
+
         
 
 
