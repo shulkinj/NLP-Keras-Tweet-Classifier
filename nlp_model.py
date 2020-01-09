85fb6fe4409847ba1d15bf823e5eb27fb61807c2
jshulkin <jshulkin@umich.edu>
2020-01-09 09:43:26 -0500

    structured previous work into a class like the keras examples

--- nlp_model.py
+++ nlp_model.py
@@ -3,19 +3,35 @@ import os
 import numpy as np
 import pandas as pd
 
-fields =['handle','text']
-df = pd.read_csv("tweets_dataset_ex1.csv", usecols=fields)
 
-#shuffle random
-np.random.seed(42)
-df = df.reindex(np.random.permutation(df.index))
-print(len(df))
-#sets how to split data
-mask = np.random.rand(len(df)) < 0.8
-trainDF = pd.DataFrame(df[mask])
-testDF = pd.DataFrame(df[~mask])
 
-print(len(trainDF),len(testDF))
+class NLP_Model():
+    def __init__(self):
 
-print(testDF['handle'])
+        #loads in training and test data
+        self.load_data(0.8)
+        print("hey guys")
+
+
+    def load_data(self, p):
+        fields =['handle','text']
+        df = pd.read_csv("tweets_dataset_ex1.csv", usecols=fields)
+        
+        #shuffle random
+        np.random.seed(42)
+        df = df.reindex(np.random.permutation(df.index))
+        
+        #sets how to split data, decimal is hyperparameter
+        mask = np.random.rand(len(df)) < p
+        trainDF = pd.DataFrame(df[mask])
+        testDF = pd.DataFrame(df[~mask])
+
+        print(len(trainDF),len(testDF))
+        
+        
+
+
+
+if __name__ == '__main__':
+    model = NLP_Model()
 
